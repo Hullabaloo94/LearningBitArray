f38166ef52e4eed137e83ea900eab5475af9017f
Jason Hall <jasonahall1994@gmail.com>
2019-11-14 12:22:53 +0000

    initial commit

---
+++ LearningBitArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace LearningBitArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] preload = new bool[3] { true, false, true };
+            
+            /**enemyGrid[0] = false;
+            enemyGrid[1] = true;
+            enemyGrid.Set(2, false);**/
+
+            BitArray enemyGrid = new BitArray(preload);
+
+            foreach (var gridItem in enemyGrid)
+            {
+                Console.WriteLine(gridItem);
+            }
+
+        }
+    }
+}
